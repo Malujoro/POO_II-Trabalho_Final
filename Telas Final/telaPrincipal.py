151e32b4e4828b778163c5455dd4a9e0b18fd7d3
Malujoro <mateus_rocha_sousa@hotmail.com>
2025-01-13 15:38:02 -0300

    Execução das telas de cesta

--- Telas Final/telaPrincipal.py
+++ Telas Final/telaPrincipal.py
@@ -857,6 +857,7 @@ class Ui_MainWindow(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" color:#ffffff;\">Instagram<br />Facebook</span></p></body></html>"))
+
 import images_rc
 
 if __name__ == "__main__":
