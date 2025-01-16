0c0f962ecbddb16cacd7304876c3ae6b038eabae
VivySilva <77069795+VivySilva@users.noreply.github.com>
2025-01-16 20:12:17 -0300

    Update reserva.py

--- bd/entities/reserva.py
+++ bd/entities/reserva.py
@@ -24,43 +24,72 @@ class Reserva:
         self._data_limite = data_limite
         self._medicamentos = medicamentos
 
-    # Getters e Setters
     @property
     def reserva_id(self):
+        """
+        Retorna o identificador único da reserva.
+        """
         return self._reserva_id
 
     @reserva_id.setter
     def reserva_id(self, value):
+        """
+        Define o identificador único da reserva.
+        """
         self._reserva_id = value
 
     @property
     def cpf_cliente(self):
+        """
+        Retorna o CPF do cliente que realizou a reserva.
+        """
         return self._cpf_cliente
 
     @cpf_cliente.setter
     def cpf_cliente(self, value):
+        """
+        Define o CPF do cliente que realizou a reserva.
+        """
         self._cpf_cliente = value
 
     @property
     def nome_cliente(self):
+        """
+        Retorna o nome do cliente que realizou a reserva.
+        """
         return self._nome_cliente
 
     @nome_cliente.setter
     def nome_cliente(self, value):
+        """
+        Define o nome do cliente que realizou a reserva.
+        """
         self._nome_cliente = value
 
     @property
     def data_limite(self):
+        """
+        Retorna a data limite da reserva.
+        """
         return self._data_limite
 
     @data_limite.setter
     def data_limite(self, value):
+        """
+        Define a data limite da reserva.
+        """
         self._data_limite = value
 
     @property
     def medicamentos(self):
+        """
+        Retorna a lista de medicamentos associados à reserva.
+        """
         return self._medicamentos
 
     @medicamentos.setter
     def medicamentos(self, value):
+        """
+        Define a lista de medicamentos associados à reserva.
+        """
         self._medicamentos = value
