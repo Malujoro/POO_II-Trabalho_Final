87dac7ded2a581eb3f80ce2e41d683ddd7048df1
Alef Cauan Sousa Rodriguês <alef.rodrigues@ufpi.edu.br>
2025-01-21 16:05:33 -0300

    ajustando

--- Telas Final/telaCesta.py
+++ Telas Final/telaCesta.py
@@ -2,13 +2,16 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import sys
 import images_rc
+import os 
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 class TelaCesta(QMainWindow):
     
     def __init__(self):
 
         super().__init__()
-        uic.loadUi("Telas Final/telaCesta.ui", self)
+        uic.loadUi(os.path.join(current_dir, "ui/telaCesta.ui"), self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
