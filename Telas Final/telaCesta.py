f57e4f1f0b0b8e3d319533164faf89c350929e60
Alef Cauan Sousa Rodriguês <alef.rodrigues@ufpi.edu.br>
2025-01-23 11:08:38 -0300

    atrapalhando

--- Telas Final/telaCesta.py
+++ Telas Final/telaCesta.py
@@ -13,23 +13,20 @@ class TelaCesta(QMainWindow):
         super().__init__()
         uic.loadUi(os.path.join(current_dir, "ui/telaCesta.ui"), self)
 
-
         # ainda não sei oq esses botões vão fazer 
         self.botao_cesta = self.findChild(QPushButton, "botaoCesta")
-        # self.botao_cesta.clicked.connect(self.func_botao_cesta)
 
         self.botao_cesta2 = self.findChild(QPushButton, "botaoCesta_2")
-        # self.botao_cesta2.clicked.connect(self.func_botao_cesta2)
+        self.botao_cesta2.clicked.connect(self.excluir_remedio)
 
-        def func_botao_cesta(self):
-            # self.nova_tela = ...
-            # self.nova_tela.show()
-            pass
 
-        def func_botao_cesta2(self):
-            # self.nova_tela = ...
-            # self.nova_tela.show()
-            pass
+    def excluir_remedio(self):
+        print("a")
+
+    def func_botao_cesta2(self):
+        # self.nova_tela = ...
+        # self.nova_tela.show()
+        pass
 
 
 if __name__ == "__main__":
--- Telas Final/telaCestaVazia.py
+++ Telas Final/telaCestaVazia.py
@@ -1,25 +1,24 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton  # Changed QDialog to QMainWindow
+from telaOfertas import TelaOfertas
 import sys
 import images_rc
 import os 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-class TelaCestaVazia(QMainWindow):  # Changed from QDialog to QMainWindow
+class TelaCestaVazia(QMainWindow):  
 
     def __init__(self):
         super().__init__()
         uic.loadUi(os.path.join(current_dir, "ui/telaCestaVazia.ui"), self)
 
-        # quando tiver as ofertas 
         self.botao_ofertas = self.findChild(QPushButton, "botaoIrparaOfertas")
-        self.botao_ofertas.clicked.connect(self.ir_ofertas_tela)
+        self.botao_ofertas.clicked.connect(self.ofertas_tela)
 
-    def ir_ofertas_tela(self):
-        # self.nova_tela = ...
-        # self.nova_tela.show()
-        pass
+    def ofertas_tela(self):
+        self.nova_tela = TelaOfertas()
+        self.nova_tela.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
