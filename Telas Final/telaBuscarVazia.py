d248d3613d82a6033303d788856d22bcc0b17dcb
Alef Cauan Sousa Rodriguês <alef.rodrigues@ufpi.edu.br>
2025-01-23 09:13:07 -0300

    feat: ajeitando as telas

--- Telas Final/telaBuscarVazia.py
+++ Telas Final/telaBuscarVazia.py
@@ -6,16 +6,13 @@ import os
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-
-class TelaGerenciar(QMainWindow):
+class TelaBuscarVazia(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi(os.path.join(current_dir, "ui/telaGerenciar.ui"), self)
-
+        uic.loadUi(os.path.join(current_dir, "ui/telaBuscarVazia.ui"), self)
 
 if __name__ == "__main__":
-    import sys
     app = QApplication(sys.argv)
-    tela = TelaGerenciar()    
+    tela = TelaBuscarVazia()    
     tela.show()
     app.exec_()
