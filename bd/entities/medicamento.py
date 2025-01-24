ab1b647dfe72f197352470dd2c40690e26c2fd23
VivySilva <77069795+VivySilva@users.noreply.github.com>
2025-01-24 00:46:03 -0300

    Update medicamento.py

--- bd/entities/medicamento.py
+++ bd/entities/medicamento.py
@@ -19,43 +19,59 @@ class Medicamento:
         self._nome = nome
         self._preco = preco
         self._quantidade_estoque = quantidade_estoque
-    
+
     @property
     def medicamento_id(self) -> int:
-        """Retorna o identificador único do medicamento."""
+        """
+        Retorna o identificador único do medicamento.
+        """
         return self._medicamento_id
 
     @medicamento_id.setter
     def medicamento_id(self, medicamento_id: int) -> None:
-        """Define o identificador único do medicamento."""
+        """
+        Define o identificador único do medicamento.
+        """
         self._medicamento_id = medicamento_id
 
     @property
     def nome(self) -> str:
-        """Retorna o nome do medicamento."""
+        """
+        Retorna o nome do medicamento.
+        """
         return self._nome
 
     @nome.setter
     def nome(self, nome: str) -> None:
-        """Define o nome do medicamento."""
+        """
+        Define o nome do medicamento.
+        """
         self._nome = nome
 
     @property
     def preco(self) -> float:
-        """Retorna o preço do medicamento."""
+        """
+        Retorna o preço do medicamento.
+        """
         return self._preco
 
     @preco.setter
     def preco(self, preco: float) -> None:
-        """Define o preço do medicamento."""
+        """
+        Define o preço do medicamento.
+        """
         self._preco = preco
 
     @property
     def quantidade_estoque(self) -> int:
-        """Retorna a quantidade em estoque do medicamento."""
+        """
+        Retorna a quantidade em estoque do medicamento.
+        """
         return self._quantidade_estoque
 
     @quantidade_estoque.setter
     def quantidade_estoque(self, quantidade_estoque: int) -> None:
-        """Define a quantidade em estoque do medicamento."""
+        """
+        Define a quantidade em estoque do medicamento.
+        """
         self._quantidade_estoque = quantidade_estoque
